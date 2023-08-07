957907dcb6f9eb9b4a61ecc7cb966d377b6f1226
Ronaldo Barros <ronaldobarros1008@gmail.com>
2023-08-07 18:28:09 -0300

    form create

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model'
 import { ProductService } from './../product.service'
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
@@ -10,6 +11,10 @@ import { Router } from '@angular/router'
 export class ProductCreateComponent implements OnInit {
 
   //propLegal = "qualquer!!!"
+  product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(private productService: ProductService,
     private router: Router) { }
@@ -20,7 +25,12 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage("Produto criado!!!")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto criado!!!")
+      this.router.navigate(['products/'])
+    })
+
+    
   }
 
   cancel(): void {
