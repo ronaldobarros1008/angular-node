f9e85c6cabff8d1ca71dd10c726cac2746df97be
Ronaldo Barros <ronaldobarros1008@gmail.com>
2023-08-07 20:29:36 -0300

    schematics + pagina read

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -31,4 +31,8 @@ export class ProductService {
     return this.http.post<Product>(this.baseUrl, product)
   }
 
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
 }
